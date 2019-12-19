a3e0a2436ee3c2e5039cadd7e4b11de87b7813b1
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2019-12-18 21:49:10 -0500

    service finished, controller in progress

--- src/main/java/com/theocho/youflix/controllers/VideoController.java
+++ src/main/java/com/theocho/youflix/controllers/VideoController.java
@@ -1,5 +1,17 @@
 package com.theocho.youflix.controllers;
 
 
+import com.theocho.youflix.services.VideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class VideoController {
+
+    @Autowired
+    private VideoService videoService;
+
+    @PostMapping
+    public UploadFileResponse
 }
--- src/main/java/com/theocho/youflix/repositories/VideoRepository.java
+++ src/main/java/com/theocho/youflix/repositories/VideoRepository.java
@@ -1,4 +1,9 @@
 package com.theocho.youflix.repositories;
 
-public class VideoRepository {
+import com.theocho.youflix.models.VideoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VideoRepository extends JpaRepository<VideoModel,String> {
 }
