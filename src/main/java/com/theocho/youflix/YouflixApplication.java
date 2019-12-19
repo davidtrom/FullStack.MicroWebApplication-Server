e322609c160e74e5e7482ed972294634e6f18d75
michael-doll <MWDoll88@gmail.com>
2019-12-18 19:40:56 -0500

    spring project, devTools, web

---
+++ src/main/java/com/theocho/youflix/YouflixApplication.java
@@ -0,0 +1,13 @@
+package com.theocho.youflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouflixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/theocho/youflix/YouflixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theocho.youflix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouflixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
