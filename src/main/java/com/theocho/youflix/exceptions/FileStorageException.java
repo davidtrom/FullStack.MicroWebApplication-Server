fb45822aebb2c91ecae21431cf3de73d12205f36
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2019-12-18 23:34:22 -0500

    added exceptions package and two classes

---
+++ src/main/java/com/theocho/youflix/exceptions/FileStorageException.java
@@ -0,0 +1,11 @@
+package com.theocho.youflix.exceptions;
+
+public class FileStorageException extends RuntimeException {
+    public FileStorageException(String message) {
+        super(message);
+    }
+
+    public FileStorageException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
