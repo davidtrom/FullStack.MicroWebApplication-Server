cee9b27d98dc0f745111e608ccc0b3099306ef60
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2020-01-04 16:35:33 -0500

    security failure

--- src/main/java/com/theocho/youflix/services/ProfileServices.java
+++ src/main/java/com/theocho/youflix/services/ProfileServices.java
@@ -1,9 +1,14 @@
 package com.theocho.youflix.services;
 
+
+import com.theocho.youflix.repositories.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfileServices {
 
+    @Autowired
+    private ProfileRepository profileRepository;
 
 }
