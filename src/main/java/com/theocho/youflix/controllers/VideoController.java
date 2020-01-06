b0b85a693ab7a92cdac55d7a5c87dc031b966f52
Ron Duwell <ronduwell@zipcoders-MacBook-Pro-4.local>
2020-01-06 09:10:50 -0500

    took security out of pom

--- src/main/java/com/theocho/youflix/controllers/VideoController.java
+++ src/main/java/com/theocho/youflix/controllers/VideoController.java
@@ -2,7 +2,10 @@ package com.theocho.youflix.controllers;
 
 import com.theocho.youflix.models.Video;
 import com.theocho.youflix.services.VideoServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.logging.LoggerGroup;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -13,10 +16,12 @@ import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = {"http://localhost:4200", "http://localhost:8080", "http://localhost"})
 public class VideoController {
 
     private VideoServices videoServices;
+    public static final Logger LOGGER = LoggerFactory.getLogger(VideoController.class);
+
 
     @Autowired
     public VideoController(VideoServices videoServices) {
@@ -25,6 +30,7 @@ public class VideoController {
 
     @PostMapping("/videos")
     public ResponseEntity<Video> createVideo(@RequestBody Video video) {
+        LOGGER.info("Request recieved");
         return new ResponseEntity(videoServices.create(video), HttpStatus.CREATED);
     }
 
--- src/main/java/com/theocho/youflix/models/Video.java
+++ src/main/java/com/theocho/youflix/models/Video.java
@@ -39,19 +39,19 @@ public class Video {
         this.title = title;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
     public String getDescription() {
         return description;
     }
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
     }
 }
