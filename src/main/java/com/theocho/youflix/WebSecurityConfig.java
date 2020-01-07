5e8d9565398fbce19b0fb26f790144c5ebb9c752
michael-doll <MWDoll88@gmail.com>
2020-01-07 17:37:57 -0500

    security cors

--- src/main/java/com/theocho/youflix/WebSecurityConfig.java
+++ src/main/java/com/theocho/youflix/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.theocho.youflix;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.Ordered;
@@ -27,30 +28,15 @@ import java.util.Arrays;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins("http://localhost:4200","https://angular-youflix-videoapp.herokuapp.com/");
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
     }
 
-//    public void addResourceHandlers(final ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-//    }
-
-    protected void configure(HttpSecurity http) throws Exception{
-        http
-                // your security config here
-                .authorizeRequests()
-                .antMatchers(HttpMethod.TRACE, "/**").denyAll()
-                .antMatchers("/admin/**").authenticated()
-                .anyRequest().permitAll()
-                .and().httpBasic()
-                .and().headers().frameOptions().disable()
-                .and().csrf().disable()
-                .headers()
-                // the headers you want here. This solved all my CORS problems!
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Origin", "*"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Methods", "GET, DELETE, PUT, POST"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Max-Age", "3600"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Credentials", "true"))
-                .addHeaderWriter(new StaticHeadersWriter("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization"));
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new
+                UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
     }
 }
