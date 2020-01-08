3db5c2fdeaac107384d8e404916b1c3b37c38f63
michael-doll <MWDoll88@gmail.com>
2020-01-08 10:04:01 -0500

    update webconfig, testing

--- src/main/java/com/theocho/youflix/WebConfig.java
+++ src/main/java/com/theocho/youflix/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("*", "https://angular-youflix-videoapp.herokuapp.com", "https://safe-springs-62086.herokuapp.com/videos", "https://safe-springs-62086.herokuapp.com", "https://angular-youflix-videoapp.herokuapp.com/videos", "https://github.com", "https://s3-S3_REGION_ID.amazonaws.com/S3_BUCKET/SampleVideo_1280x720_5mb.mp4")
+                .allowedOrigins("*", "https://angular-youflix-videoapp.herokuapp.com", "https://safe-springs-62086.herokuapp.com/videos", "https://safe-springs-62086.herokuapp.com", "https://angular-youflix-videoapp.herokuapp.com/videos", "https://github.com", "https://s3-S3_REGION_ID.amazonaws.com/zcw-group-videos/**")
                 .allowedMethods("POST")
                 .allowedHeaders("Content-Type", "Authorization")
                 .allowCredentials(false)
