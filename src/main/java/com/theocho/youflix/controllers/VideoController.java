a150dfb09920e9cd491b33e0b2992d8598adfbc1
michael-doll <MWDoll88@gmail.com>
2020-01-07 20:40:31 -0500

    update videocontroller, testing crossorigin getmapping videos/id

--- src/main/java/com/theocho/youflix/controllers/VideoController.java
+++ src/main/java/com/theocho/youflix/controllers/VideoController.java
@@ -28,7 +28,7 @@ public class VideoController {
         LOGGER.info("Request received");
         return new ResponseEntity(videoServices.create(video), HttpStatus.CREATED);
     }
-    
+    @CrossOrigin(origins = {"https://angular-youflix-videoapp.herokuapp.com, https://safe-springs-62086.herokuapp.com, https://safe-springs-62086.herokuapp.com/videos, https://angular-youflix-videoapp.herokuapp.com/videos" })
     @GetMapping("/videos/{id}")
     public ResponseEntity<Video> show(@PathVariable Long id) {
         return new ResponseEntity<>(videoServices.showOne(id), HttpStatus.OK);
