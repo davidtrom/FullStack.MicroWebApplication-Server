7a4aa324e45fffeba6197aa95a3c55da9ec5e79e
Jack Harris <jack@zipcoders-MacBook-Pro-49.local>
2020-01-10 12:45:07 -0500

    turning in mac

--- src/test/java/com/theocho/youflix/controllers/ProfileControllerTest.java
+++ src/test/java/com/theocho/youflix/controllers/ProfileControllerTest.java
@@ -1,8 +1,16 @@
 package com.theocho.youflix.controllers;
 
+import com.theocho.youflix.models.Profile;
+import com.theocho.youflix.models.Video;
+import com.theocho.youflix.repositories.ProfileRepository;
+import com.theocho.youflix.repositories.VideoRepository;
+import com.theocho.youflix.services.ProfileServices;
+import com.theocho.youflix.services.VideoServices;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -13,10 +21,27 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import static org.junit.Assert.*;
 
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(ProfileController.class)
 public class ProfileControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    ProfileServices profileServices;
+
+    @InjectMocks
+    ProfileController profileController;
+
+    @MockBean
+    ProfileRepository profileRepository;
+    Profile testProfile;
 
     @Before
     public void setUp() throws Exception {
+        testProfile = new Profile("testUser","testPass1","test@test.com");
     }
 
     @After
